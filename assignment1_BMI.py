2b21e0bc40322bacd1571f9e2a66ff04ffbda2a6
unknown <hastishirazianorg@gmail.com>
2023-12-05 08:52:07 -0800

    my first project

---
+++ assignment1_BMI.py
@@ -0,0 +1,27 @@
+a= float(input("please enter your weight:"))
+b= float(input("please enter your height:"))
+#The desired calculation unit for weight is kilograms and the calculation unit for height is meters.
+
+bmi= a/b**b
+
+if bmi<18.5:
+    result= "under weight"
+
+elif 18.5<= bmi <=24.9:
+    result= "normal weight"
+
+elif 25<= bmi <=29.9:
+    result= "over weight"
+
+elif 30<= bmi <=34.9:
+    result= "obesity"
+
+elif 35<= bmi <=39.9:
+    result= "extreme obesity"
+
+elif 40<bmi:
+    result= "not in the range."
+
+
+print("Your BMI is:",bmi)
+print("status:", result)
