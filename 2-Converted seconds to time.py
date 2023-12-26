11cd17a879e5fe9f287482b1d205c409027e2fe1
Hasti Shirazian <152734085+hastishirazian@users.noreply.github.com>
2023-12-26 18:19:34 +0330

    Update 2-Converted seconds to time.py

--- 2-Converted seconds to time.py
+++ 2-Converted seconds to time.py
@@ -1,11 +1,14 @@
 print("Please enter the number:")
 x = int(input("seconds:"))
 
-hour = x/3600
+hour = x//3600
 y = x%3600
 
-minute = y/60
+minute = y//60
 seconds = y%60
 
-#hour can be 
-print(" Time:" , int(hour) ,":", int(minute) , ":" , seconds)
+#hour can be a number between 1 to 24
+#minute can be a number between 0 to 6
+#second can be a number between 0 to 60
+
+print(" Time:" , hour ,":", minute , ":" , seconds)
