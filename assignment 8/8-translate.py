635ab73106596fbf4826655e6be9e8a6f577dbbd
unknown <hastishirazianorg@gmail.com>
2024-03-17 02:02:29 +0330

    pylearn

--- assignment 8/8-translate.py
+++ assignment 8/8-translate.py
@@ -1,3 +1,4 @@
+import os
 import gtts
 
 def read_from_file():
@@ -22,28 +23,43 @@ def show_menu():
 def persian_to_english():
     user_input = input("Enter your persian text:")
     user_words = user_input.split(" ")
+    user_sentence = user_text.split(".")
+    print(user_input)
+    output = ""
 
     for user_word in user_words:
         for word in words_bank:
             if user_word == word["fa"]:
-                print(word["en"])
+                output = output + word["en"] + " "
                 break
         else:
-            print(user_word)
+            output = output + user_word + " "
+    
+    print(output + ".")
 
+    sound = gtts.gTTS(output , lang = "en" , slow = False )
+    sound.save("Assignment_8/voice_fa_1.mp3")
 
 def english_to_persian():
     user_input = input("Enter your english text:")
     user_words = user_input.split(" ")
+    user_sentence = user_text.split(".")
+    print(user_input)
+    output = ""
 
     for user_word in user_words:
         for word in words_bank:
             if user_word == word["en"]:
-                print(word["fa"])
+                output = output + word["fa"] + " "
                 break
         else:
-            print(user_word)
+            output = output + user_word + " "
 
+    output = output + user_word + " "
+
+    sound2 = gtts.gTTS(output , lang = "ur" , slow = False )
+    sound2.save("Assignment_8/voice_en_1.mp3")
+    
 def add_word():
     print("In which language do you want to add a new word?")
     print("1. English")
@@ -79,10 +95,17 @@ def add_word():
             my_dict = {"en":translated_word , "fa":new_word}
             words_bank.append(my_dict)
             print("The new word was successfully added to the database✅")
+
 ############################################################ functions
+#voice
+my_text = "hello my name is hasti."
+
+x = gtts.gTTS(my_text , lang = "en" , slow = False )
+x.save("Assignment 8/voice1.mp3")
+
 read_from_file() 
 print("Welcome to my translate!!!!")
-    
+
 while True:
     show_menu()
  
