3df46fd097021692d89a1cb1f0c40a00c4803909
unknown <hastishirazianorg@gmail.com>
2024-05-27 01:34:40 +0330

    pylearn

--- assignment 10/10-class fraction.py
+++ assignment 10/10-class fraction.py
@@ -1,26 +1,38 @@
-class franction:
+class fraction:
 
     #properties
-    def __init__(self,numerator,Denominator):
-        numerator = n   #soorat
-        Denominator = d #makhraj
+    def __init__(self,n,d):
+        self.numerator = n   #soorat
+        self.Denominator = d #makhraj
 
 
     #methods
     def sum(self):
         ...
 
-    def Multiplied(self):
-        ...
-    
-    def Minus(self):
+    def mul(self , fraction1 , fraction2):
+        result1 = fraction1.numerator*fraction2.numerator
+        result2 = fraction1.Denominator*fraction2.Denominator
+        x = fraction(result1,result2)
+        return x
+
+    def sub(self):
         ...
 
-    def Divided(self):
+    def Div(self):
         ...
     
-    def convert_to_AdadMakhloot(self):
+    def fraction_to_number(self):
         ...
 
-franc = franction(6,8)
-print(franc.numerator , "/" , franc.Denominator)
+    def show(self):
+        print(self.numerator , "/" , self.Denominator)
+
+a= fraction(6,8)
+a.show()
+
+b= fraction(7,1)
+b.show()
+
+z= a.mul(a,b)
+z.show()
